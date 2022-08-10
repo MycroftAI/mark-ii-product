cb33dc8a52805e66f539489d69347043ba3a6571
Kris Gesling <kris.gesling@mycroft.ai>
2022-08-10 21:44:50 +0930

    rm old install scripts

--- build/01-mycroft/install-mimic3.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#
-# Install Mimic 3 TTS plugin.
-# Voices are in /etc/mycroft/mimic3/voices/
-#
-
-set -euo pipefail
-
-/home/mycroft/.config/mycroft/.venv/bin/pip3 install /etc/mycroft/mimic3/mimic3-*.tar.gz
--- build/01-mycroft/install-precise.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#
-# Install Precise Lite plugin.
-# Model is included in plugin distribution.
-#
-
-set -euo pipefail
-
-/home/mycroft/.config/mycroft/.venv/bin/pip3 install /etc/mycroft/precise-lite/hotword_precise_lite-*.tar.gz
