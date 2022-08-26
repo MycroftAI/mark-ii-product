d14c16884a6ca196761e6d28eef948cd85850f32
Michael Hansen <mike@rhasspy.org>
2022-08-26 10:51:09 -0500

    Restart mpd service when device is auto mounted to avoid /media permission problem

--- docker/files/usr/bin/udisks-automount.sh
+++ docker/files/usr/bin/udisks-automount.sh
@@ -8,5 +8,14 @@ stdbuf -oL -- udevadm monitor --udev -s block | while read -r -- _ _ event devpa
         if [ "$event" = add ]; then
             devname=$(pathtoname "$devpath")
             udisksctl mount --block-device "$devname" --no-user-interaction
+
+            # HACK
+            # mpd complains of permission problems regarding /media.
+            # Running as the "mycroft" user does not help, nor does altering the
+            # umask/dmask options during mount.
+            #
+            # Restarting the mpd service fixes the problem immediately for some
+            # reason, so that's what we're doing here.
+            systemctl restart mpd || true
         fi
 done
