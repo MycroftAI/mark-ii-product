6e8e6a1cd3f04ea62e8a4f64c83e05c25f7281f1
Michael Hansen <mike@rhasspy.org>
2022-09-02 11:53:09 -0500

    Remove duplicate GUI files (in mycroft-dinkum/services/gui)

--- docker/build/gui/build-mycroft-gui.sh
+++ docker/build/gui/build-mycroft-gui.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 mkdir -p mycroft-gui/builddir
 cd mycroft-gui/builddir
 
-cmake ../ -DCMAKE_INSTALL_PREFIX=/usr \
+cmake ../ -DCMAKE_INSTALL_PREFIX=/usr/local \
     -DCMAKE_BUILD_TYPE=Release -DKDE_INSTALL_LIBDIR=lib \
     -DKDE_INSTALL_USE_QT_SYS_PATHS=on
 
