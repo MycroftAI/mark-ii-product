3fc06af79d8bc7be1a317fdcebbe0479b5da01f0
Michael Hansen <mike@rhasspy.org>
2022-09-28 11:06:56 -0500

    Set initial XMOS microphone parameters to help AEC

--- mark-ii-raspberrypi/files/opt/mycroft/xmos-microphone/run.sh
+++ mark-ii-raspberrypi/files/opt/mycroft/xmos-microphone/run.sh
@@ -34,5 +34,26 @@ sleep 1
 # Upload XMOS firmware directly
 "${venv_dir}/bin/python3" "${this_dir}/send_image_from_rpi.py" --direct "${this_dir}/app_xvf3510_int_spi_boot_v4_2_0.bin"
 
+# ---------------------------
+# Set initial XMOS parameters
+# ---------------------------
+
+# Disable alternative architecture.
+#
+# This keeps AEC (acoustic echo cancellation) and the IC (interference
+# cancellation) enabled at all times.
+vfctrl_i2c --no-check-version SET_ALT_ARCH_ENABLED 0
+
+# Lower the max gain to avoid the device hearing itself.
+# This happens despite the AEC being enabled, unfortunately.
+vfctrl_i2c --no-check-version SET_MAX_GAIN_CH0_AGC 150
+
+# Start the auto delay estimator (ADEC) off closer to the actual values it will
+# eventually settle of after calibration. The device automatically calibrates
+# when it first plays audio, but these values are initially way off the mark.
+vfctrl_i2c --no-check-version SET_DELAY_DIRECTION 1
+vfctrl_i2c --no-check-version SET_DELAY_SAMPLES 120
+
+
 # Set up Ti Amp (speakers)
 "${venv_dir}/bin/python3" "${this_dir}/tas5806Test.py"
